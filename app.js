b680b73a8343a04a6122fb38874586d78445df52
CarlosSalega <carlosalfredosalega@gmail.com>
2023-04-17 11:31:59 -0200

    1st commit

---
+++ app.js
@@ -0,0 +1,12 @@
+require("colors");
+const { showMenu } = require("./helpers/message");
+
+console.clear();
+
+const main = async() => {
+  console.log("Hello World 2");
+
+  showMenu();
+}
+
+main();
---
+++ helpers/message.js
@@ -0,0 +1,12 @@
+require("colors");
+
+const showMenu = () => {
+  console.clear();
+  console.log("======================".green)
+  console.log("   Select an Option".green)
+  console.log("======================".green)
+}
+
+module.exports = {
+  showMenu,
+}
