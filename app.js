ca8b1c208bccdd2b303cb92f9ba2f3765b2b07cb
CarlosSalega <carlosalfredosalega@gmail.com>
2023-04-18 10:45:14 -0200

    4th commit

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 require("colors");
 
-const {inquirerMenu} = require("./helpers/inquirer");
+const { inquirerMenu, pause } = require("./helpers/inquirer.js");
 
 console.clear();
 
-const main = async() => {
-  console.log("Hello World");
-
-  let answer = "";
+const main = async () => {
+  let answer = "0";
 
   do {
-
     answer = await inquirerMenu();
-    console.log({answer});
-    
+    console.log({ answer });
+    await pause();
   } while (answer !== "0");
-
-}
+};
 
 main();
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -6,23 +6,64 @@ const questions = [
     type: "list",
     name: "option",
     message: "What do you want to do?",
-    choices: ["option1", "option2", "option3"]
-  }
-]
+    choices: [
+      {
+        value: "1",
+        name: "1. Create task",
+      },
+      {
+        value: "2",
+        name: "2. List task",
+      },
+      {
+        value: "3",
+        name: "3. List completed tasks",
+      },
+      {
+        value: "4",
+        name: "4. List pending tasks",
+      },
+      {
+        value: "5",
+        name: "5. Complete task(s)",
+      },
+      {
+        value: "6",
+        name: "6. Delete task",
+      },
+      {
+        value: "0",
+        name: "7. Exit",
+      },
+    ],
+  },
+];
 
 const inquirerMenu = async () => {
-
   console.clear();
   console.log("======================".green);
   console.log("   Select an Option".green);
   console.log("======================\n".green);
 
-  const option = await inquirer.prompt(questions);
+  const { option } = await inquirer.prompt(questions);
 
   return option;
+};
 
-}
+const pause = async () => {
+  const question = [
+    {
+      type: "input",
+      name: "enter",
+      message: `\nPress ${"ENTER".green} to continue\n`,
+    },
+  ];
+
+  console.log("\n");
+  await inquirer.prompt(question);
+};
 
 module.exports = {
   inquirerMenu,
-}
+  pause,
+};
